5d9c781acb11314fbad0b058c53055acc6fcd728
Ian <ianrizh900@gmail.com>
2025-04-08 16:22:49 +0800

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div style={{ padding: '2rem' }}>
+        <h1>About This App</h1>
+        <p>This is a simple test deployment for Fly.io.</p>
+        </div>
+    );
+}  
